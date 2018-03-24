f516f6a9ab9de010faddb7acc1f4130e8ca38532
Li Arolf Rey <liarolfrrey@gmail.com>
2018-03-24 22:14:13 +0800

    fix: Only deploy MatchBetting contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-const MatchFactory = artifacts.require('./MatchFactory.sol');
 const MatchBetting = artifacts.require('./MatchBetting.sol');
 
 module.exports = function (deployer) {
-  deployer.deploy(MatchFactory);
   deployer.deploy(MatchBetting);
 };
