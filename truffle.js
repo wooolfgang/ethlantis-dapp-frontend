dbdfedd89282f7a019e2bf6a37b53a975a303cc7
Li Arolf Rey <liarolfrrey@gmail.com>
2018-06-04 22:11:05 +0800

    feat: Deploy to ropsten network

--- truffle.js
+++ truffle.js
@@ -1,5 +1,15 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const mnemonic = 'execute ankle fiscal laundry voyage kiwi rich scan chalk defense risk glory';
+
 module.exports = {
   networks: {
+    ropsten: {
+      provider() {
+        return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/4j50CPIg7m1Fp24GLDrR');
+      },
+      network_id: 3,
+    },
     development: {
       host: '127.0.0.1',
       port: 7545,
