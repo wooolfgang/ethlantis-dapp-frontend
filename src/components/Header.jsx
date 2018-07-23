40558e0cbde9e20631845d98870267c3edc02a6a
Godfrey Zubiaga <godfreyzubiaga@gmail.com>
2018-07-23 08:03:01 +0800

    style: adjust and fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,13 @@ import Nav from './header//Nav';
 const StyledHeader = styled.nav`
   display: grid;
   grid-template-areas: '. flex .';
-  grid-template-columns: 50px 1fr 50px;
+  grid-template-columns: 20px 1fr 30px;
   height: 64px;
   background: ${props => props.theme.colorPrimary};
+
+  @media screen and (max-width: 700px) {
+    grid-template-columns: 10px 1fr 20px;
+  }
 `;
 
 const Flex = styled.div`
--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -4,17 +4,35 @@ import Link from '../Link';
 
 const StyledNav = styled.div`
   position: relative;
+  color: white;
 
   a {
     color: white !important;
+  }
+
+  @media screen and (min-width: 700px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    width: 200px;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  transition-duration: 0.2s;
+  font-weight: 500;
+
+  &:hover {
+    transform: scale(1.1);
+    transiton-duration: 0.2s;
   }
 `;
 
 const Nav = () => (
   <StyledNav >
-    <Link to="/matches" href="/" > Matches </Link>
-    <Link to="/faq" href="/"> Faq </Link>
-    <Link to="/login" href="/"> Login </Link>
+    <StyledLink to="/matches" href="/" > Matches </StyledLink> |
+    <StyledLink to="/faq" href="/"> Faq </StyledLink> |
+    <StyledLink to="/login" href="/"> Login </StyledLink>
   </StyledNav >
 );
 
